e88047986aae72a19d98236bfdfdc333b4b0354f
anivi <anivi98@hotmail.com>
2019-09-08 12:32:06 -0500

    agregue nuevas cositas y arregle un error

---
+++ java.js
@@ -0,0 +1,11 @@
+/*window.onload=function() {
+    var left=document.getElementById("sticky");
+    var stop=left.offsetTop;
+     
+    window.onscroll=function() {
+    var scrollTop=(window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+     
+    if (scrollTop >= stop) {left.className="sticky"}
+    else {left.className=""}
+    }
+}*/
