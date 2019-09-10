55163e5a3b4981444e04f44d27d4901a857e60ec
anivi <anivi98@hotmail.com>
2019-09-10 18:50:41 -0500

    galeria y caracteristicas arregladas

--- menu.js
+++ menu.js
@@ -10,4 +10,34 @@
         
         }
     }
-  
+/*
+    function showText(){
+        var text = document.getElementsByClassName('landing__carac-tex');
+        text.style.opacity = "1"; 
+    }
+*/
+
+
+    var fotos = document.querySelectorAll('.landing__galeria-foto');
+    var fotoMostrar = document.querySelector('.landing__galeria-mostrar');
+    
+function recorrerFotos(foto, index){
+
+    function clickFoto(event){
+        var urlImg = foto.style.backgroundImage;
+        var urlImgSplit = urlImg.split('/');
+
+        var nombrePunto = urlImgSplit[urlImgSplit.length - 1].split('.');
+        var nombre = nombrePunto[nombrePunto.length-2];
+
+        fotoMostrar.style.backgroundImage = " url('./imagenes/" + nombre + ".png')";
+        fotoMostrar.style.transition = "all ease-in 0.3s";
+        console.log(nombre);
+
+    }
+    foto.addEventListener('click', clickFoto);
+    
+} fotos.forEach(recorrerFotos);
+
+
+  //  console.log(url + "url de la imagen");
